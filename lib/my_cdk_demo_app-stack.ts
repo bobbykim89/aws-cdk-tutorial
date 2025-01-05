a9d6a8482cc2afeec7c342201fcb567075763f5f
Sihun Kim <skim585@asu.edu>
2025-01-04 23:28:08 -0700

    feat(*): initialization
    initialization with basic config.

--- lib/my_cdk_demo_app-stack.ts
+++ lib/my_cdk_demo_app-stack.ts
@@ -1,10 +1,11 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib'
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3'
+import { Construct } from 'constructs'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MyCdkDemoAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // The code that defines your stack goes here
 
@@ -12,5 +13,18 @@ export class MyCdkDemoAppStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'MyCdkDemoAppQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // L1 and L2 Construct of an S3 Bucket
+    const level1S3Bucket = new CfnBucket(this, 'MyFirstLevel1ConstructBucket', {
+      versioningConfiguration: {
+        status: 'Enabled',
+      },
+    })
+
+    const level2S3Bucket = new Bucket(this, 'MyFirstLevel2ConstructBucket', {
+      versioned: true,
+      bucketName: 'my-first-level2-construct-bucket-for-real-pio-pio',
+      // removalPolicy: cdk.RemovalPolicy.DESTROY
+    })
   }
 }
